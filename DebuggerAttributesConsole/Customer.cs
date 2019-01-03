159a2921045b97ce657cacaf15dcc3233054a8ba
pdejonghe <dejonghe.peter@gmail.com>
2019-01-03 12:03:32 +0100

    Initial commit

---
+++ DebuggerAttributesConsole/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DebuggerAttributesConsole
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfFirstOrder { get; set; }
+        public string City { get; set; }
+
+        public ICollection<CustomerOrder> Orders { get; } = new List<CustomerOrder>();
+
+        public void PlaceOrder(string productCode, int quantity, decimal productPrice)
+        {
+            Orders.Add(new CustomerOrder(productCode, quantity, productPrice));
+        }
+
+        public void PlaceOrder(Product product, int quantity)
+        {
+            this.PlaceOrder(product.Code, quantity, product.Price);
+        }
+    }
+}
---
+++ DebuggerAttributesConsole/CustomerOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DebuggerAttributesConsole
+{
+    public class CustomerOrder
+    {
+        public Guid CustomerOrderId { get; set; }
+
+        public string ProductCode { get; set; }
+        public int Quantity { get; set; }
+        public decimal ProductPrice { get; set; }
+
+        public CustomerOrder(string code, int quantity, decimal price)
+        {
+            this.ProductCode = code;
+            this.Quantity = quantity;
+            this.ProductPrice = price;
+        }
+
+        public decimal CalculateOrderTotal()
+        {
+            return Quantity * ProductPrice;
+        }
+    }
+}
